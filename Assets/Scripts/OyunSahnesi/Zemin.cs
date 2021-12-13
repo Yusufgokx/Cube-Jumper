384568289ab2a327fcfe3d9fd1ed6710ba86bf6f
Yusufgokx <91879973+Yusufgokx@users.noreply.github.com>
2021-12-13 18:54:27 +0300

    Zemin Spawn
    sdgsdg

--- Assets/Scripts/OyunSahnesi/Zemin.cs
+++ Assets/Scripts/OyunSahnesi/Zemin.cs
@@ -8,12 +8,18 @@ public class Zemin : MonoBehaviour
 
 
     private void OnCollisionEnter2D(Collision2D collision){
-       Rigidbody2D rb = collision.collider.GetComponent<Rigidbody2D>();
-        if (rb != null){
-            Vector2 ZýplamaVelocity = rb.velocity;
-            ZýplamaVelocity.y = ZýplamaKuvveti;
-            rb.velocity = ZýplamaVelocity;
+        if (collision.relativeVelocity.y < 0) 
+        {
+            Rigidbody2D rb = collision.collider.GetComponent<Rigidbody2D>();
+            if (rb != null)
+            {
+                Vector2 ZýplamaVelocity = rb.velocity;
+                ZýplamaVelocity.y = ZýplamaKuvveti;
+                rb.velocity = ZýplamaVelocity;
+            }
         }
+
+       
     }
 
 
