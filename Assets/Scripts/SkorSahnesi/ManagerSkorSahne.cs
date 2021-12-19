91686dd19b4dc31c9a6213543b6d44e185a5d95f
Yusufgokx <91879973+Yusufgokx@users.noreply.github.com>
2021-12-20 00:09:43 +0300

    DF
    DF

---
+++ Assets/Scripts/SkorSahnesi/ManagerSkorSahne.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ManagerSkorSahne : MonoBehaviour
+{
+
+    private void Update()
+    {
+        {
+            if (Input.GetMouseButton(0))
+            {
+                SceneManager.LoadScene(0);
+            }
+        }
+    }
+}
